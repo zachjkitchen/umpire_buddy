5b8c4a523294e6f8fd51530cf512f2a9d2b61d84
zachjkitchen <43613624+zachjkitchen@users.noreply.github.com>
2020-02-09 20:59:20 -0600

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -3,19 +3,27 @@ package com.example.umpire_buddy;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 import android.view.View;
+import android.widget.Button;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView showValue;
+    TextView showStrikes = findViewById(R.id.strikeCount);
+    TextView showBalls = findViewById(R.id.ballCount);
+
     int strikes = 0;
     int balls = 0;
+
+
+    Button btn1 = findViewById(R.id.strikeButton);
+    Button btn2 = findViewById(R.id.ballButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        showValue = (TextView) findViewById(R.id.strikeCount);
+        showStrikes = findViewById(R.id.strikeCount);
+        showBalls = findViewById(R.id.ballCount);
 
 
         super.onCreate(savedInstanceState);
@@ -23,20 +31,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void callStrike(View view) {
+
         strikes++;
-        showValue.setText(Integer.toString(strikes));
+        showStrikes.setText(Integer.toString(strikes));
+        if (strikes > 2) {
+            btn1.setEnabled(false);
+            btn2.setEnabled(false);
+        }
     }
 
     public void callBall(View view) {
         balls++;
-        showValue.setText(Integer.toString(balls));
+        showBalls.setText(Integer.toString(balls));
+        if (balls > 3) {
+            btn2.setEnabled(false);
+            btn1.setEnabled(false);
+        }
     }
 
     public void resetCount (View view) {
         strikes = 0;
         balls = 0;
-        showValue.setText(String.valueOf(strikes));
-        showValue.setText((String.valueOf(balls)));
+        showStrikes.setText(String.valueOf(strikes));
+        showBalls.setText((String.valueOf(balls)));
+    }
+
+    public void showAbout (View view) {
+        showStrikes.setText("Umpire Buddy 2.0");
+        showBalls.setText("Zach Kitchen");
     }
 
 }
